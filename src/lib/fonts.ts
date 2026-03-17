bed9483bd335b660df4b0b11218d3dc0221e7aa8
Kenny Egan <kenegan2005@gmail.com>
2026-03-17 14:32:29 -0400

    update lib

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,14 +2,17 @@ import { DM_Mono, Manrope, Sora } from 'next/font/google';
 
 export const manrope = Manrope({
   subsets: ['latin'],
+  display: 'swap',
 });
 
 export const sora = Sora({
   subsets: ['latin'],
   weight: ['600', '700'],
+  display: 'swap',
 });
 
 export const dmMono = DM_Mono({
   subsets: ['latin'],
   weight: ['400', '500'],
+  display: 'swap',
 });
